5b576230eab618177140cd38debad0cc459d4db4
Hermyo <yohann.hermeline@gmail.com>
2018-08-28 23:04:39 +0200

    modify second paragraph text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Hi there ! I'm happy to be here today :-)</p>
+    <p>Hi there ! I'm happy to be here today !!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
